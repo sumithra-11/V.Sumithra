365bc8d6c69021f755a9fc7f3c6acfaeeb266b52
sumithra-11 <86637259+sumithra-11@users.noreply.github.com>
2021-06-28 22:17:16 -0400

    .

---
+++ Vowelconsonent.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Vowelconsonant
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a character");
+    char ch=sc,next().charAt(0);
+    switch (ch) {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+            System.out.println(ch+" is vowel");
+                break;
+            default:
+                System.out.println(ch+" is consonant");
+        }
+    }
+}
